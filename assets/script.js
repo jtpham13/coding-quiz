1cfb8c1e80f526b5f9114b2fc7f230c1dbe0f27a
Joey Pham <joeypham@joeys-mbp.mynetworksettings.com>
2023-04-13 17:07:59 -0400

    adding correct and incorrect questions

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,9 @@ var startPage = document.querySelector("#quiz-page")
 var quizQuestions = document.querySelector("#quiz-questions")
 var answerChoicesList = document.querySelector("#answer-choices")
 var quizContainer= document.getElementById("quiz-container")
-var index = 0
+var isCorrect = document.getElementById('isCorrect')
 
-startQuiz.addEventListener('click', beginQuiz) 
-
-function beginQuiz (){
-    console.log("Quiz started")
-   startPage.style.display = "none"
-   showQuestions(index)
-}
-
+// Quiz Questions
 var questions = [
     {
         questionText: "Which of the following characters surround an array?", 
@@ -34,62 +27,117 @@ var questions = [
 
     }
 ]
- 
-function showQuestions(index){
-    
-       
-        quizQuestions.innerHTML = questions[index].questionText
-        var answerChoicesArr = questions[index].answerChoices
-        answerChoicesArr.forEach((answer, index)=>{
-            var answerListTag = document.createElement('li');
-            answerListTag.innerHTML = answer
-            console.log(answerListTag)
-            answerChoicesList.appendChild(answerListTag)
-        })
-        var nextBtn= document.createElement("button")
-        nextBtn.innerHTML="next"
-        nextBtn.addEventListener('click', function(){
-                console.log('next btn clicked')
-            
-            })
-            quizContainer.append(nextBtn)
 
+// Landing page to start quiz
+startQuiz.addEventListener('click', beginQuiz) 
+
+function beginQuiz (){
+    console.log("Quiz started")
+   startPage.style.display = "none"
+showQuestions()
+}
+
+
+ 
+function showQuestions(){
     
+ 
+// displaying question 1   
+quizQuestions.textContent = questions[0].questionText
+
+
+
+var answerChoicesArray= questions[0].answerChoices
+
+answerChoicesArray.forEach(function(answer){
+console.log(answer)
+
+
+var answerListTag=document.createElement("li")
+ 
+
+
+answerListTag.innerHTML=answer
+console.log(answerListTag)
+
+answerListTag.classList.add("liststyle")
+answerListTag.addEventListener('click', (event)=>{
+    var userAnswer = event.target.innerHTML
+    if(userAnswer === questions[0].correctAnswer){
+        console.log('correct')
+        isCorrect.innerHTML = "Correct"
+    } else{
+        isCorrect.innerHTML = 'Incorrect'
+    }
+})
+
+answerChoicesList.appendChild(answerListTag)
+
+}) 
+
+var nextBtn= document.createElement("button")
+console.log(nextBtn)
+nextBtn.innerHTML="next"
+console.log(quizContainer)
+nextBtn.addEventListener('click', function(){
+    showQuestion2();
+
+})
+quizContainer.append(nextBtn)
+
+};
+
+
+function checkAnswer(){
+    console.log('clicked')
+}
+
+
+
+
+
+
+
+
+function showQuestion2() {
+
+    quizQuestions.textContent = questions[1].questnsionText
+
+
+
+    var answerChoicesArray= questions[1].answerChoices
+    
+    answerChoicesArray.forEach(function(answer,index){
+    console.log(answer)
+    
+    
+    var answerListTag=document.createElement("li")
+     
+    
+    
+    answerListTag.innerHTML=`${index+1}. ${answer}`
+    console.log(answerListTag)
+    
+    answerListTag.classList.add("liststyle")
+
    
-// quizQuestions.textContent = questions[0].questionText
-
-
-
-// var answerChoicesArray= questions[0].answerChoices
-
-// answerChoicesList= answerChoicesArray.forEach(function(answer,index){
-// console.log(answer)
-
-
-// var answerListTag=document.createElement("li")
- 
-
-
-// answerListTag.innerHTML=`${index+1}. ${answer}`
-// console.log(answerListTag)
-
-// answerListTag.classList.add("liststyle")
-
-// answerChoicesList.appendChild(answerListTag)
-
-// })
-
-// var nextBtn= document.createElement("button")
-// console.log(nextBtn)
-// nextBtn.innerHTML="next"
-// console.log(quizContainer)
-// nextBtn.addEventListener('click', function(){
-//     console.log('next btn clicked')
-
-// })
-// quizContainer.append(nextBtn)
+    
+    answerChoicesList.appendChild(answerListTag)
+    
+    })
+    
+    var nextBtn= document.createElement("button")
+    console.log(nextBtn)
+    nextBtn.innerHTML="next"
+    console.log(quizContainer)
+    nextBtn.addEventListener('click', function(){
+        console.log('next')
+    
+    })
+    quizContainer.append(nextBtn)
 
 }
+
    
 
 
